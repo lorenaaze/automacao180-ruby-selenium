f0b744aa22ed5d744b025b9b0b2fab6f0d54b7e2
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2025-05-09 15:50:59 -0300

    adicionando cenários de cadastro e faker para criação de e-mail automático

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,15 +1,54 @@
 Dado('que acesso a página de cadastro') do
-    visit "http://rocklov-web:3000/signup"
+  visit "http://rocklov-web:3000/signup"
 end
   
-  Quando('submeto o meu cadastro completo') do
-    find("#fullName").set "Fernando Papito"
-    find("#email").set "fernando@email.com"
-    find("#password").set "pwd123"
-    click_button "Cadastrar"
+Quando('submeto o meu cadastro completo') do
+  find("#fullName").set "Fernando Papito"
+  find("#email").set Faker::Internet.email
+  find("#password").set "pwd123"
+  click_button "Cadastrar"
 end
   
-  Então('sou redirecionado para o dashboard') do
-    expect(page).to have_css ".dashboard"
-    sleep 10
+Então('sou redirecionado para o dashboard') do
+  expect(page).to have_css ".dashboard"
+end
+
+Quando('submeto o meu cadastro sem o nome') do
+  find("#email").set Faker::Internet.email
+  find("#password").set "pwd123"
+  click_button "Cadastrar"
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe seu nome completo!"
+end
+
+Quando('submeto o meu cadastro sem o email') do
+  find("#fullName").set "Fernando Papito"
+  find("#password").set "pwd123"
+  click_button "Cadastrar"
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe um email válido!"
+end
+
+Quando('submeto o meu cadastro com email incorreto') do
+  find("#fullName").set "Fernando Papito"
+  find("#email").set "123"
+  find("#password").set "pwd123"
+  click_button "Cadastrar"
+end
+
+Quando('submeto o meu cadastro sem a senha') do
+  find("#fullName").set "Fernando Papito"
+  find("#email").set Faker::Internet.email
+  click_button "Cadastrar"
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe sua senha secreta!"
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,7 @@ require "capybara"
 require "capybara/cucumber"
 require 'selenium-webdriver'
 require 'webdrivers'
-
+require 'faker'
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
